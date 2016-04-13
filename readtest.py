03a022ca74231b257f8d4744fa222ed55e8512e5
ZhangHao <ywzhang909@hotmail.com>
2016-04-13 23:01:01 +0800

    seems to be going to work... or not...

--- readtest.py
+++ readtest.py
@@ -10,23 +10,27 @@ def sigmoid(x):
 
 def IRSL(train_x, train_y, eps=0.0001):
     n_samples, n_features = shape(train_x)
-    w = mat(0.01 * ones(n_features, float))
+    w = mat(0.01 * ones(n_features, float)).T
     diag = mat(zeros([n_samples, n_samples]))
     print('start fitting...')
     while True:
         yp = sigmoid(train_x * w)
         error = yp - train_y
         for i in range(n_samples):
-            diag[(i, i)] = dot(yp[i], (1 - yp[i]))
-        H = dot(dot(train_x.T, diag), train_x)
-        wn = dot(dot(dot(H.I, train_x.T), diag), (dot(train_x, w) - dot(diag.getI(), error)))
-        sub_error = wn - w
-        max_error = 0
-        for e in sub_error.flat:
-            max_error = max(max_error, abs(e))
-        if max_error < eps:
-            break
-        w = wn
+            diag[(i, i)] = yp[i] * (1 - yp[i])
+        diag_inv = mat(zeros([n_samples, n_samples]))
+        for i in range(n_samples):
+            diag_inv[(i, i)] = 1 / diag[(i, i)]
+        H = train_x.T * diag * train_x
+        print(H)
+        # wn = H.I * train_x.T * diag * ( train_x * w - diag.I * error)
+        # sub_error = wn - w
+        # max_error = 0
+        # for e in sub_error.flat:
+        #     max_error = max(max_error, abs(e))
+        # if max_error < eps:
+        #     break
+        # w = wn
     print('fitting success!')
     return w
 
@@ -35,7 +39,7 @@ def accuracy(test_x, test_y, w):
     n_samples, n_features = shape(test_x)
     match_count = 0
     for i in range(n_samples):
-        predict = sigmoid(dot(test_x[i, :], w))[0, 0] > 0.5
+        predict = sigmoid(test_x[i, :] * w)[0, 0] > 0.5
         if predict == (test_y[i, :] == 1):
             match_count += 1
     return float(match_count) / n_samples
